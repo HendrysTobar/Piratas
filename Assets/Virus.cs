8db34454102f491c98f4ecbcc8020c1d08273eb7
Hendrys Tobar <fabiantobar@gmail.com>
2018-05-11 00:20:57 -0500

    Archivos de demo de instanciacion y corutinas

---
+++ Assets/Virus.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Virus : MonoBehaviour 
+{
+	#region Enums
+	
+	
+    #endregion
+	
+    #region Atributos y Propiedades
+	
+	
+    #endregion
+	
+    #region Eventos    
+	
+	
+    #endregion
+	
+    #region Mensajes Unity
+	
+	void Start () {
+		
+	}
+
+    Quaternion rotacionObjetivo;
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Translate( Time.deltaTime * transform.forward);
+
+        float angle = Random.Range(-10, 10);
+        transform.Rotate(Vector3.up, angle, Space.World);
+        
+		
+	}
+	#endregion
+	
+    #region Métodos
+	
+	
+    #endregion
+    #region CoRutinas
+	
+	
+	#endregion
+}
---
+++ Assets/VirusSpawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusSpawner : MonoBehaviour 
+{
+    #region Enums
+
+
+    #endregion
+
+    #region Atributos y Propiedades
+    public GameObject virusPrefab;
+	
+    #endregion
+	
+    #region Eventos    
+	
+	
+    #endregion
+	
+    #region Mensajes Unity
+	
+	void Start ()
+    {
+        StartCoroutine(CorutinaEngendrarViruses());		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    #endregion
+
+    #region Métodos
+    public void SpawnVirus()
+    {
+        Instantiate(virusPrefab, this.transform.position,virusPrefab.transform.rotation);
+    }
+
+    public IEnumerator CorutinaEngendrarViruses()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3f);
+            SpawnVirus();
+        }
+    }
+	
+    #endregion
+    #region CoRutinas
+	
+	
+	#endregion
+}
